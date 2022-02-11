d187ab561919ff90bbe41cbcf40b42f754fd5fda
nuxler-coder <97177938+nuxler-coder@users.noreply.github.com>
2022-02-11 16:41:10 +0600

    Update wgcodes.py

--- wgcodes.py
+++ wgcodes.py
@@ -1,9 +1,8 @@
 # Welcome message code
 @client.event
 async def on_member_join(member):
-  guild = client.get_guild(918042903002099743)
-  channel = guild.get_channel(930001374660395019)
-  channel2 = guild.get_channel(938678975667007499)
+  guild = client.get_guild(# paste your server's id here)
+  channel = guild.get_channel(# paste your server's welcome channel's id here)
   text = "WELCOME"
   name = str(member)
 
@@ -14,8 +13,8 @@ async def on_member_join(member):
 
   draw = ImageDraw.Draw(img)
   pfp= circle(pfp, (385, 389))
-  font = ImageFont.truetype("Redwing-Medium.ttf", 78)
-  font2 = ImageFont.truetype("Redwing-Medium.ttf", 100)
+  font = ImageFont.truetype("Redwing-Medium.otf", 78)
+  font2 = ImageFont.truetype("Redwing-Medium.otf", 100)
   draw.text((500, 499), text, font=font)
   draw.text((430, 560), name, font=font2)
   img.paste(pfp, (435, 80), pfp)
@@ -25,7 +24,6 @@ async def on_member_join(member):
     a.seek(0)
     file = discord.File(a, "wlc.png")
   await channel.send(f"**Hey{member.mention}!**")
-  await channel2.send(f"**Hey {member.mention}! Type `$verify` or `$verifyMe` to complete your verification and have the access to this server!**")
   wlcEmbed = discord.Embed(title="Welcome!", description=f"?? Welcome to **{guild.name}** {member.mention}!\n?? Now we have {guild.member_count} members!\nThanks for joining us!\n? Complete your verification in <#938678975667007499> to join the rest of this server.", color=discord.Color.blue())
   wlcEmbed.set_footer(text="Enjoy your time staying here!")
   wlcEmbed.set_image(url="attachment://wlc.png")
@@ -34,8 +32,8 @@ async def on_member_join(member):
 # Goodbye message code
 @client.event
 async def on_member_remove(member):
-  guild = client.get_guild(918042903002099743)
-  channel = guild.get_channel(930001391429230602)
+  guild = client.get_guild(# paste your server's id here)
+  channel = guild.get_channel(# paste your server's goodbye channel's id here)
   text = "GOODBYE"
   name = str(member)
 
@@ -46,8 +44,8 @@ async def on_member_remove(member):
 
   draw = ImageDraw.Draw(img)
   pfp= circle(pfp, (385, 385))
-  font = ImageFont.truetype("Redwing-Medium.ttf", 68)
-  font2 = ImageFont.truetype("Redwing-Medium.ttf", 100)
+  font = ImageFont.truetype("Redwing-Medium.otf", 68)
+  font2 = ImageFont.truetype("Redwing-Medium.otf", 100)
   draw.text((500, 499), text, font=font)
   draw.text((430, 560), name, font=font2)
   img.paste(pfp, (435, 80), pfp)
